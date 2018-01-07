97299e395a5a976f2384a3ef16cf19bce9fd9255
ay9010x <33930428+ay9010x@users.noreply.github.com>
2018-01-07 10:08:01 +0800

    Update 1.js

--- config/1.js
+++ config/1.js
@@ -1,1 +1,48 @@
 
+/**
+ * 適用網頁：http://vinson.rd.ssic.nccu.edu.tw/
+ * 事件查詢表：https://docs.google.com/spreadsheets/d/1MtMtw9lKLDTUzfBd6Ld0fAe_FGe5u-Mlkh5WfZiH5qM/edit
+ * @author Pudding 20170203
+ */
+
+GA_TRACE_CODE = "UA-112072858-1";
+        
+var _local_debug = false;
+
+if (_local_debug === true) {
+    CSS_URL = "https://localhost/GA-project/config/1.css";
+    LIB_URL = "https://localhost/GA-project/1.js";
+    console.log("[LOCAL TEST MODE]");
+}
+else {
+    CSS_URL = "https://ay9010x.github.io/GA-project/config/1.css";
+    LIB_URL = "https://ay9010x.github.io/GA-project/config/1.js";
+}
+
+
+var exec = function () {
+    auto_set_user_id();   
+
+    //按鈕
+    ga_mouse_click_event("#btn_1","Click Btn_1");
+    ga_mouse_click_event(".btn","Click Btn");
+
+    //滑鼠滑入滑出
+    ga_mouse_over_event(".img","Hover img");
+
+    //表單送出
+    ga_submit_event("form", "submit", function (_ele) {
+        return _ele.text();});
+    
+};
+
+
+// --------------------------------------
+
+$(function () {
+    $.getScript(LIB_URL, function () {
+        ga_setup(function () {
+            exec();
+        });
+    });
+});
