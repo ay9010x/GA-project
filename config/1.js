8e9fcaf72e9e4ced910f282b290153404b67bf3b
ay9010x <33930428+ay9010x@users.noreply.github.com>
2018-01-07 11:00:50 +0800

    Update 1.js

--- config/1.js
+++ config/1.js
@@ -46,3 +46,13 @@ $(function () {
         });
     });
 });
+
+
+//----------
+
+$(function() {  
+    var len =  $('#theMenu').find('.topmenu').length; 
+    for(var i=0;i<len;i++){
+        $('#theMenu').find('.topmenu').eq(i).attr('id', 'top_menu'+i);    
+    };
+});
